76f433348be93f9a58304b0ba29faf4b43ceed22
yvesSiewe <yvesrolandsiewe@gmail.com>
2025-11-15 04:53:56 +0100

    ajout de la vue

---
+++ todoapp/lib/view/todo_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todoapp/viewmodel/todo_viewmodel.dart';
+class TodoView extends ConsumerWidget {
+  const TodoView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final todoPage = ref.watch(todoProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('todoApp'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                TextFormField(),
+                IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(Icons.add)
+                )
+              ],
+            ),
+            ListView.builder(
+              itemBuilder: (context, index){
+                if(todoPage.isEmpty){
+                  return Text('Ajoutez une tache');
+                }
+                return Row(
+                  children: [
+                    Checkbox(
+                      value: todoPage[index].isDone,
+                      onChanged: (value){
+                        ref.read(todoProvider.notifier).toggleDone(index);
+                      }
+                    ),
+                    Text(todoPage[index].todo),
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.remove),
+                    )
+                  ],
+                );
+              }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
